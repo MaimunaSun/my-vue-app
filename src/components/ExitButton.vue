b3be6196588c78f293384b362484d23217a1c264
MaimunaSun <104510174+MaimunaSun@users.noreply.github.com>
2024-02-18 20:27:40 +0200

    commit

--- src/components/ExitButton.vue
+++ src/components/ExitButton.vue
@@ -1,29 +1,28 @@
 <template>
-    <button class="exit-button" @click="exit">Exit</button>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      exit() {
-        // Close the window when the exit button is clicked
-        window.close();
-      }
+  <button class="exit-button" @click="exit">Exit</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    exit() {
+      // Navigate to the StartPage route when the exit button is clicked
+      this.$router.push({ name: 'Start' });
     }
   }
-  </script>
-  
-  <style scoped>
-  .exit-button {
-    background-color: #ff0000; /* Red */
-    color: #fff; /* White */
-    border: none;
-    border-radius: 4px;
-    padding: 10px 20px;
-    cursor: pointer;
-    position: fixed;
-    top: 20px;
-    right: 20px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.exit-button {
+  background-color: #ff0000; /* Red */
+  color: #fff; /* White */
+  border: none;
+  border-radius: 4px;
+  padding: 10px 20px;
+  cursor: pointer;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+}
+</style>
