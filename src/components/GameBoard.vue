5cb30523194015d02b9f07be6ab311a9f5025a47
MaimunaSun <104510174+MaimunaSun@users.noreply.github.com>
2024-02-18 14:46:19 +0200

    commit

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -62,8 +62,8 @@ export default {
   margin-top: 20px; /* Add space between the heading/exit button and game board layout */
   display: flex;
   justify-content: space-around;
-  width: 800px; /* Adjust width as needed */
-  height: 400px; /* Adjust height as needed */
+  width: 1200px; /* Adjust width as needed */
+  height: 500px; /* Adjust height as needed */
 }
 
 
